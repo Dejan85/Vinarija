77ef73ef0aa4ae12f6a9ed2a013e245b2031ca16
vanjadu <vanja.dulikravic@icloud.com>
2021-05-17 18:24:07 +0200

    test

--- src/components/frontend/footer/Footer.tsx
+++ src/components/frontend/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = (): JSX.Element => {
-  return <div>Footer jedan dva tri</div>;
+  return <div>Footer jedan dva tri, cetiri pet sest</div>;
 };
 
 export default Footer;
