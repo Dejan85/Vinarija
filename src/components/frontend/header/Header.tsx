b3160a14f193a43bd5ccf4d1d2f44e430c3c41fe
Dejan85 <xypnotica@gmail.com>
2021-05-17 17:22:59 +0200

    update

--- src/components/frontend/header/Header.tsx
+++ src/components/frontend/header/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Button } from "src/components/ui";
 
-const Header = ():JSX.Element => {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+const Header = (): JSX.Element => {
+  return (
+    <div>
+      Logo
+      <nav>nav</nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
