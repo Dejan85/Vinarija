3edc06555eb949836248223cc63fb061e7c3e951
Dejan85 <xypnotica@gmail.com>
2021-05-17 15:48:24 +0200

    update

--- src/components/frontend/footer/Footer.tsx
+++ src/components/frontend/footer/Footer.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-<<<<<<< HEAD
 const Footer = (): JSX.Element => {
-  return <div>Footer xad</div>;
+  return <div>Footer jedan dva tri</div>;
 };
-=======
-const Footer = ():JSX.Element => {
-    return (
-        <div>
-            Footer
-            jedan dva tri
-        </div>
-    )
-}
->>>>>>> 5dd951487e1d5d98049214a29d448fb2d3d0388e
 
 export default Footer;
