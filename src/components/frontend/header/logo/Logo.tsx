e0ee6beb711679e30c148508078ae09ec7926e69
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-17 20:34:05 +0200

    update

--- src/components/frontend/header/logo/Logo.tsx
+++ src/components/frontend/header/logo/Logo.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = (): JSX.Element => {
   return (
-    <Image
-      src="/images/logo/logo.png"
-      alt="logo"
-      width="200px"
-      height="60px"
-      objectFit="contain"
-    />
+    <Link href="/">
+      <Image
+        src="/images/logo/logo.png"
+        alt="logo"
+        width="200px"
+        height="60px"
+        objectFit="contain"
+        className="logo"
+      />
+    </Link>
   );
 };
 
--- src/components/frontend/header/shop/Shop.tsx
+++ src/components/frontend/header/shop/Shop.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Shop = (): JSX.Element => {
   return (
-    <Image src="/icons/shop/shop.svg" alt="img" width="50px" height="50px" />
+    <Link href="/shop">
+      <Image
+        src="/icons/shop/shop.svg"
+        alt="img"
+        width="50px"
+        height="50px"
+        className="shop"
+      />
+    </Link>
   );
 };
 
