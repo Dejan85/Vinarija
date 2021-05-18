f2321ffcb803875bff576f4b5f59c0eafaf81f81
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-18 21:21:38 +0200

    created home-about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { WelcomePage } from "src/components/frontend";
+import { WelcomePage, HomeAbout } from "src/components/frontend";
 
 const Index = (): JSX.Element => {
   return (
     <>
       <WelcomePage />
+      <HomeAbout />
     </>
   );
 };
--- src/components/frontend/home/home-about/HomeAbout.tsx
+++ src/components/frontend/home/home-about/HomeAbout.tsx
@@ -4,15 +4,15 @@ import { SubContainer } from "src/components/ui";
 const HomeAbout = () => {
   return (
     <SubContainer as="section" className="home-about">
-      <h1>O nama</h1>
-      <p>
+      <h1 className="home-about__heading">O nama</h1>
+      <p className="home-about__text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos at nulla
         tempora ipsum voluptatibus magnam aut veniam, eos adipisci veritatis
         enim accusantium nostrum molestias voluptate blanditiis nobis. Qui,
         perspiciatis sit.
       </p>
 
-      <p>
+      <p className="home-about__text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos at nulla
         tempora ipsum voluptatibus magnam aut veniam, eos adipisci veritatis
         enim accusantium nostrum molestias voluptate blanditiis nobis. Qui,
