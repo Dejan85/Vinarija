180552bcacec87334a5586f2dfe2d52544b670c3
vanjadu <vanja.dulikravic@icloud.com>
2021-05-18 20:20:49 +0200

    meta tags

--- src/components/frontend/layout/Layout.tsx
+++ src/components/frontend/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
 import { Header, Footer } from "src/components/frontend";
-// import Favicon from '../../../../public/icons/favicon.svg'
 
-const Layout = ({ children, title = "Vinarija Rnjak | Dobrodošli" }): JSX.Element => {
+const Layout = ({
+  children,
+  title = "Vinarija Rnjak | Dobrodošli",
+}): JSX.Element => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -13,8 +15,23 @@ const Layout = ({ children, title = "Vinarija Rnjak | Dobrodošli" }): JSX.Eleme
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="apple-touch-icon" sizes="180x180" href="../../../public/icons/favicons/apple-touch-icon.png" />
-        <link rel="shortcut icon" type="image/png" sizes="32x32" href="../../../public/icons/favicons/favicon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicons/favicon.png"
+        />
+        <meta name="description" content="Vinarija Rnjak" />
+        <meta name="author" content="Dejan Markovic, Vanja Dulikravic" />
+        <meta
+          name="keywords"
+          content="vino, vina, vinograd, vinogradi, rose, chardonnay, sauvignon blanc, pinot noir, cabernet sauvignon, merlot"
+        />
       </Head>
       <Header />
       {children}
