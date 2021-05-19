2628a73df6c5bb446e77e26eea4fcf66735f1f3c
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-19 17:54:20 +0200

    added toggle nav

--- src/components/frontend/header/nav/Nav.tsx
+++ src/components/frontend/header/nav/Nav.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Squash as Hamburger } from "hamburger-react";
 
 const Nav = (): JSX.Element => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const toggleHandler = () => {
+    setToggleNav(!toggleNav);
+  };
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--navigation-width",
+      `${toggleNav ? "100%" : "0%"}`
+    );
+  }, [toggleNav]);
+
   return (
     <nav className="nav">
-      <Hamburger />
+      <Hamburger onToggle={toggleHandler} />
     </nav>
   );
 };
--- src/components/frontend/index.ts
+++ src/components/frontend/index.ts
@@ -4,4 +4,5 @@ export { WelcomePage } from "./home/welcome-page";
 export { HomeAbout } from "./home/home-about";
 export { HomeProducts } from "./home/home-products";
 export { HomeGallery } from "./home/home-gallery";
+export { Navigation } from "./navigation";
 export { Footer } from "./footer";
