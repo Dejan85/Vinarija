dc59834607945597583ac1cde43d7e5b7843652e
vanjadu <vanja.dulikravic@icloud.com>
2021-05-19 18:12:08 +0200

    wine

--- src/components/frontend/home/home-products/HomeProducts.tsx
+++ src/components/frontend/home/home-products/HomeProducts.tsx
@@ -7,14 +7,27 @@ const Products = (): JSX.Element => {
   return (
     <>
       <SubContainer className="products" as="section">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          heading="Chardonnay"
+          image="/images/home/products/wine-1.png"
+        />
+        <Product
+          heading="Sauvignon Blanc"
+          image="/images/home/products/wine-2.png"
+        />
+        <Product
+          heading="Cabernet Sauvignon"
+          image="/images/home/products/wine-3.png"
+        />
+        <Product heading="Merlot" image="/images/home/products/wine-4.png" />
+        <Product
+          heading="Pinot Noir"
+          image="/images/home/products/wine-5.png"
+        />
+        <Product heading="Rose" image="/images/home/products/wine-6.png" />
       </SubContainer>
       <Link href="/">
-        <a className="products__link">Pogledajte vise</a>
+        <a className="products__link">Pogledajte više</a>
       </Link>
     </>
   );
--- src/components/frontend/home/home-products/partials/Product.tsx
+++ src/components/frontend/home/home-products/partials/Product.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Image from "next/image";
 
-const Product = (): JSX.Element => {
+const Product = (props): JSX.Element => {
+  const { image, heading } = props;
   return (
     <div className="product">
-      <Image
-        src="/images/home/products/vino.png"
+      {image && (
+        <Image
+        className="product__image"
+        src={image}
         alt="img"
         width=""
-        height="500px"
-        objectFit="contain"
+        height="650px"
+        objectFit="cover"
       />
-      <h3 className="product__heading">PINOT NOIR</h3>
+      )}
+      <h3 className="product__heading">{heading}</h3>
     </div>
   );
 };
