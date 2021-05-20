b8e64ece7a1e4f41da2e2e6c494c736e7699608b
vanjadu <vanja.dulikravic@icloud.com>
2021-05-20 21:27:41 +0200

    navigation completed

--- src/components/frontend/header/nav/Nav.tsx
+++ src/components/frontend/header/nav/Nav.tsx
@@ -1,28 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Squash as Hamburger } from "hamburger-react";
+import { Power3, TimelineMax } from "gsap";
 
 const Nav = (): JSX.Element => {
   const [toggleNav, setToggleNav] = useState(false);
+  const [hamburgerColor, setHamburgerColor] = useState("red");
 
   const toggleHandler = () => {
     setToggleNav(!toggleNav);
   };
 
-  useEffect(() => {
-    document.documentElement.style.setProperty(
-      "--navigation-width",
-      `${toggleNav ? "100%" : "0%"}`
-    );
+  const tl = new TimelineMax();
 
-    document.documentElement.style.setProperty(
-      "--global-overflow",
-      `${toggleNav ? "hidden" : "auto"}`
-    );
+  useEffect(() => {
+    if (toggleNav) {
+      tl.to(".navigation", { left: "0%", ease: Power3.easeOut });
+      tl.staggerFrom(
+        ".navigation__col__nav-links",
+        0.5,
+        { x: -200, opacity: 0 },
+        0.3
+      );
+      tl.staggerFrom(
+        ".navigation__col__socials",
+        0.5,
+        { y: 100, opacity: 0 },
+        0.2
+      );
+      tl.fromTo(
+        ".navigation__col__nav-images",
+        { opacity: 0, transform: "scale(0.8)" },
+        { opacity: 1, transform: "scale(1)", ease: Power3.easeOut },
+        1.5
+      );
+      setHamburgerColor("#fff");
+    } else {
+      tl.to(".navigation", { left: "-100%", ease: Power3.easeOut });
+      setHamburgerColor("#1d1d1d");
+    }
   }, [toggleNav]);
 
   return (
     <nav className="nav">
-      <Hamburger onToggle={toggleHandler} />
+      <Hamburger color={hamburgerColor} onToggle={toggleHandler} />
     </nav>
   );
 };
--- src/components/frontend/navigation/Navigation.tsx
+++ src/components/frontend/navigation/Navigation.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Image from "next/image";
 
 const Navigation = (): JSX.Element => {
-  return <div className="navigation">navigation</div>;
+  return (
+    <div className="navigation">
+      <div className="navigation__col">
+        <Image
+          className="navigation__col__nav-logo"
+          src="/images/logo/logo_2.png"
+          alt="logo"
+          width="200px"
+          height=""
+          objectFit="contain"
+          draggable="false"
+        />
+        <a className="navigation__col__nav-links" href="#">
+          Naslovna
+        </a>
+        <a className="navigation__col__nav-links" href="#">
+          Vina
+        </a>
+        <a className="navigation__col__nav-links" href="#">
+          Vinogradi
+        </a>
+        <a className="navigation__col__nav-links" href="#">
+          Prodavnica
+        </a>
+        <a className="navigation__col__nav-links" href="#">
+          Kontakt
+        </a>
+        <div className="navigation__col__socials">
+          <p>Konektujmo se</p>
+          <div className="navigation__col__socials__icons">
+            <a href="#">
+              <img src="/icons/socials/facebook.svg" alt="facebook" />
+            </a>
+            <a href="#">
+              <img src="/icons/socials/instagram.svg" alt="instagram" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="navigation__col">
+        <img
+          className="navigation__col__nav-images"
+          src="images/home/welcome-page/background.webp"
+          alt="image"
+          draggable="false"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
