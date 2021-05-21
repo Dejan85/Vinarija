89bcc2d189eee82ff39ad60337f9a13c1d8dbad0
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-21 12:34:37 +0200

    added footer basic stlyE

--- src/components/frontend/footer/Footer.tsx
+++ src/components/frontend/footer/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Element, Paragraph, SubContainer } from "src/components/ui";
 
 const Footer = (): JSX.Element => {
-  return <div>Footer jedan dva tri, cetiri pet sest, sedam</div>;
+  return (
+    <Element as="div" className="footer">
+      <SubContainer className="footer__container">
+        <Paragraph as="p" className="footer__text">
+          Gde Kupiti
+        </Paragraph>
+        <Paragraph as="p" className="footer__text">
+          Pravila o privatnosti
+        </Paragraph>
+        <Paragraph as="p" className="footer__text footer__text--company-name">
+          Rtanj Vinarija
+        </Paragraph>
+        <Paragraph as="p" className="footer__text">
+          Kontakt
+        </Paragraph>
+      </SubContainer>
+    </Element>
+  );
 };
 
 export default Footer;
