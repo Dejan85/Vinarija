6e33a22b63ee4c97c9ce86f8506110e0e20ad987
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-21 15:07:31 +0200

    update

--- src/components/frontend/vines/Vines.tsx
+++ src/components/frontend/vines/Vines.tsx
@@ -4,7 +4,9 @@ import { SubContainer, Heading } from "src/components/ui";
 const Vine = (): JSX.Element => {
   return (
     <SubContainer className="vine">
-      <Heading as="h2">Nasa \n Kolekcija Vina</Heading>
+      <Heading as="h2" className="vine__heading">
+        Nasa <br /> Kolekcija Vina
+      </Heading>
       <h1>vine page</h1>
     </SubContainer>
   );
