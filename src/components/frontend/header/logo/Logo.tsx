8afb2a23dd2c75ccfc4ec48fdb98ac82d1e96354
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-21 11:18:27 +0200

    refactored logo

--- src/components/frontend/header/logo/Logo.tsx
+++ src/components/frontend/header/logo/Logo.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import { Image, Link } from "src/components/ui";
 
 const Logo = (): JSX.Element => {
   return (
     <Link href="/">
-      <a>
-        <Image
-          src="/images/logo/logo.png"
-          alt="logo"
-          width="150px"
-          height="60px"
-          objectFit="contain"
-          className="logo"
-          draggable="false"
-        />
-      </a>
+      <Image
+        src="/images/logo/logo.png"
+        alt="logo"
+        width="150px"
+        height="60px"
+        objectFit="contain"
+        className="logo"
+      />
     </Link>
   );
 };
