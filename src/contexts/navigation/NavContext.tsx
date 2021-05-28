3994221ebe829e0a832e996b2eefb504544c3c4e
vanjadu <vanja.dulikravic@icloud.com>
2021-05-28 14:45:42 +0200

    jkwnndf

--- src/contexts/navigation/NavContext.tsx
+++ src/contexts/navigation/NavContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { Power3, TimelineMax } from "gsap";
+import { Power3, TimelineMax, Back } from "gsap/dist/gsap";
 
 const NavContext = createContext(null);
 
@@ -18,20 +18,14 @@ const NavProvider = ({ children }) => {
       tl.staggerFrom(
         ".navigation__col__nav-links",
         0.5,
-        { x: -200, opacity: 0 },
+        { x: -200, opacity: 0, ease: Power3.easeOut },
         0.3
       );
       tl.staggerFrom(
         ".navigation__col__socials",
         0.5,
-        { y: 100, opacity: 0 },
-        0.2
-      );
-      tl.fromTo(
-        ".navigation__col__nav-images",
-        { opacity: 0, transform: "scale(0.8)" },
-        { opacity: 1, transform: "scale(1)", ease: Power3.easeOut },
-        1
+        { y: 100, opacity: 0, ease: Back.easeOut },
+        0.4
       );
     } else {
       tl.to(".navigation", { left: "-100%", ease: Power3.easeOut }, 0.1);
