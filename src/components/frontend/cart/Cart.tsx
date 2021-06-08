951fab9d9bf63f50d643a7a7c27e1ec7d2820c03
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-06-09 01:06:23 +0200

    added cart components

--- src/components/frontend/cart/Cart.tsx
+++ src/components/frontend/cart/Cart.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { SubContainer, Element, Heading, Paragraph } from "src/components/ui";
 
-const Cart = () => {
-  return <div>cart</div>;
+const Cart = (): JSX.Element => {
+  return (
+    <SubContainer className="cart" as="div">
+      <Element as="div" className="cart__heading-container">
+        <Heading as="h3">Korpa</Heading>
+        <Paragraph as="p">
+          Please review the contents of your cart. When you have finished please
+          proceed to the check out.
+        </Paragraph>
+      </Element>
+    </SubContainer>
+  );
 };
 
 export default Cart;
---
+++ src/components/frontend/cart/partials/CartTable.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartTable = (): JSX.Element => {
+  return <div></div>;
+};
+
+export default CartTable;
